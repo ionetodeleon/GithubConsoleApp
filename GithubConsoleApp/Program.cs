82bf31b634f806779de6067fb812269369a65a1c
Ignacio Oneto <ionetodeleon@gmail.com>
2024-04-01 21:15:45 -0300

    Add project files.

---
+++ GithubConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+enum DiaDeLaSemana
+{
+    Domingo = 0,
+    Lunes = 1,
+    Martes = 2,
+    Miercoles = 3,
+    Jueves = 4,
+    Viernes = 5,
+    Sabado = 6
+}
+
+class Programa
+{
+    static void Main()
+    {
+        int valorDia = int.Parse(Console.ReadLine());
+
+        DiaDeLaSemana dia = (DiaDeLaSemana)valorDia;
+        Console.WriteLine("El día de la semana es: " + dia);
+    }
+}
